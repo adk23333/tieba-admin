36ec76e1f42a9b75574a6580427e818884bd2179
adk23333 <2633103794@qq.com>
2024-02-07 03:00:16 +0800

    添加执行操作

--- tieba-admin-server/plugins/review/execute.py
+++ tieba-admin-server/plugins/review/execute.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Union
+from typing import Union, Literal
 
+from aiotieba import Client
 from aiotieba.typing import Comment as Tb_Comment
 from aiotieba.typing import Post as Tb_Post
 from aiotieba.typing import Thread as Tb_Thread
@@ -29,11 +30,12 @@ class Comment(Enum):
     Delete = 6
 
 
-UnitOptionType = Union[OptionType, User, Thread, Post, Comment]
+UnitOptionType = Union[OptionType, Thread, Post, Comment]
 
 
 @dataclass
 class Executor(object):
+    client: Client = None
     obj: Union[Tb_Thread, Tb_Post, Tb_Comment, None] = None
     user_opt: Union[User, OptionType] = OptionType.Empty
     option: UnitOptionType = OptionType.Empty
@@ -41,7 +43,25 @@ class Executor(object):
     opt_day: int = 0
 
     async def run(self):
-        ...
+        match self.user_opt:
+            case OptionType.Empty:
+                pass
+            case User.Block:
+                await self.client.block(self.obj.fid, self.obj.user.portrait, day=self.user_day)
+            case User.Black:
+                pass
+                # TODO 确定黑名单是无限循环封禁还是使用官方黑名单
+        match self.option:
+            case OptionType.Empty:
+                pass
+            case Thread.Hide:
+                await self.client.hide_thread(self.obj.fid, self.obj.tid)
+            case Thread.Delete:
+                await self.client.del_thread(self.obj.fid, self.obj.tid)
+            case Post.Delete:
+                await self.client.del_post(self.obj.fid, self.obj.tid, self.obj.pid)
+            case Comment.Delete:
+                await self.client.del_post(self.obj.fid, self.obj.tid, self.obj.pid)
 
     def exec_compare(self, exec2):
         if exec2.user_opt.value > self.user_opt.value:
@@ -60,15 +80,16 @@ def empty():
     return Executor()
 
 
-def hide(thread: Thread, day: int = 1):
+def hide(client: Client, thread: Thread, day: int = 1):
     return Executor(
+        client,
         thread,
         option=Thread.Hide,
         opt_day=day,
     )
 
 
-def delete(obj: Union[Tb_Thread, Tb_Post, Tb_Comment], day: int = 0):
+def delete(client: Client, obj: Union[Tb_Thread, Tb_Post, Tb_Comment], day: Literal[-1, 0, 1, 3, 10] = 0):
     if isinstance(obj, Tb_Thread):
         option = Thread.Delete
     elif isinstance(obj, Post):
@@ -79,29 +100,37 @@ def delete(obj: Union[Tb_Thread, Tb_Post, Tb_Comment], day: int = 0):
         option = OptionType.Empty
 
     if day:
+        if day == -1:
+            user_opt = User.Black
+        else:
+            user_opt = User.Block
         return Executor(
+            client,
             obj,
             option=option,
-            user_opt=User.Block,
+            user_opt=user_opt,
             user_day=day,
         )
     else:
         return Executor(
+            client,
             obj,
             option=option,
         )
 
 
-def block(obj: Union[Tb_Thread, Tb_Post, Tb_Comment], day: int = 1):
+def block(client: Client, obj: Union[Tb_Thread, Tb_Post, Tb_Comment], day: Literal[1, 3, 10] = 1):
     return Executor(
+        client,
         obj,
         user_opt=User.Block,
         user_day=day
     )
 
 
-def black(obj: Union[Tb_Thread, Tb_Post, Tb_Comment]):
+def black(client: Client, obj: Union[Tb_Thread, Tb_Post, Tb_Comment]):
     return Executor(
+        client,
         obj,
         user_opt=User.Black,
     )
