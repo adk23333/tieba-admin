0d2e36c3f342a47ebaa9988bb19e347e346fce59
adk23333 <2633103794@qq.com>
2024-02-13 21:35:15 +0800

    添加黑名单审核

--- tieba-admin-server/plugins/review/checker.py
+++ tieba-admin-server/plugins/review/checker.py
@@ -4,7 +4,8 @@ from typing import Union
 from aiotieba import Client
 from aiotieba.typing import Thread, Post, Comment
 
-from .execute import empty, delete
+from core.models import ForumUserPermission, Permission
+from .execute import empty, delete, block
 from .models import Keyword
 from .reviewer import Reviewer, Level
 
@@ -18,4 +19,12 @@ async def check_keyword(t: Union[Thread, Post, Comment], client: Client):
         for kw in keywords:
             if re.search(kw.keyword, t.text):
                 return delete(client, t)
+    return empty()
+
+
+@reviewer.route(['thread', 'post', 'comment'])
+async def check_black(t: Union[Thread, Post, Comment], client: Client):
+    user = await ForumUserPermission.filter(user=t.user.user_id, permission=Permission.Black.value).get_or_none()
+    if user:
+        return block(client, t, 10)
     return empty()
