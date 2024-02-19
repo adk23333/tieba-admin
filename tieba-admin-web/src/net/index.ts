5950ea9263b0c4d339e0f12a3aff35df367154c0
adk23333 <2633103794@qq.com>
2024-02-19 20:48:41 +0800

    修复bug

--- tieba-admin-web/src/net/index.ts
+++ tieba-admin-web/src/net/index.ts
@@ -5,7 +5,7 @@ import {message} from "@/plugins/toast";
 
 
 const serviceAxios = axios.create({
-  baseURL: "http://localhost:3000/api", // 基础请求地址
+  baseURL: "/api", // 基础请求地址
   timeout: 10000, // 请求超时设置
   withCredentials: false, // 跨域请求是否需要携带 cookie
 });
