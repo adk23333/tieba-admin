10d9bf4ae0864930a3af51b37b632d8bfba96d36
adk23333 <2633103794@qq.com>
2024-04-04 19:10:09 +0800

    将日志设置移入./core/log.py

---
+++ tieba-admin-server/core/log.py
@@ -0,0 +1,38 @@
+﻿import os
+import sys
+
+from sanic.log import LOGGING_CONFIG_DEFAULTS
+
+if not os.path.exists('./log'):
+    os.makedirs('./log')
+
+if not os.path.exists('./.cache'):
+    os.makedirs('./.cache')
+
+LOG_FILE_PATH = "./log/server.log"
+LOGGING_CONFIG = LOGGING_CONFIG_DEFAULTS
+LOGGING_CONFIG.update({
+    "handlers": {
+        "console": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "generic",
+            "filename": LOG_FILE_PATH,
+            'encoding': "utf-8",
+            'maxBytes': 1024 * 1024 * 1,
+            'backupCount': 10,
+        },
+        "error_console": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "generic",
+            "filename": LOG_FILE_PATH,
+            'encoding': "utf-8",
+            'maxBytes': 1024 * 1024 * 1,
+            'backupCount': 10,
+        },
+        "access_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "access",
+            "stream": sys.stdout,
+        },
+    },
+})
--- tieba-admin-server/server.py
+++ tieba-admin-server/server.py
@@ -1,14 +1,13 @@
 import logging
 import os
 import signal
-import sys
 from asyncio import sleep
 
 import aiotieba
 from argon2 import PasswordHasher
 from environs import Env
 from sanic import Sanic, Request, response
-from sanic.log import LOGGING_CONFIG_DEFAULTS, logger
+from sanic.log import logger
 from sanic.views import HTTPMethodView
 from sanic_ext import Extend
 from sanic_jwt import Initialize, protected, scoped, inject_user
@@ -16,43 +15,10 @@ from tortoise.contrib.sanic import register_tortoise
 
 from core.exception import ArgException, err_rps
 from core.jwt import authenticate, retrieve_user, JwtConfig, JwtResponse, scope_extender
+from core.log import LOGGING_CONFIG
 from core.manager import manager
 from core.models import User, Config, Permission, ForumUserPermission, ExecuteLog
 from core.utils import validate_password, get_modules, json
-
-LOG_FILE_PATH = "./log/server.log"
-LOGGING_CONFIG = LOGGING_CONFIG_DEFAULTS
-LOGGING_CONFIG.update({
-    "handlers": {
-        "console": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "generic",
-            "filename": LOG_FILE_PATH,
-            'encoding': "utf-8",
-            'maxBytes': 1024 * 1024 * 1,
-            'backupCount': 10,
-        },
-        "error_console": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "generic",
-            "filename": LOG_FILE_PATH,
-            'encoding': "utf-8",
-            'maxBytes': 1024 * 1024 * 1,
-            'backupCount': 10,
-        },
-        "access_console": {
-            "class": "logging.StreamHandler",
-            "formatter": "access",
-            "stream": sys.stdout,
-        },
-    },
-})
-
-if not os.path.exists('./log'):
-    os.makedirs('./log')
-
-if not os.path.exists('./.cache'):
-    os.makedirs('./.cache')
 
 app = Sanic("tieba-admin-server", log_config=LOGGING_CONFIG)
 Extend(app)
