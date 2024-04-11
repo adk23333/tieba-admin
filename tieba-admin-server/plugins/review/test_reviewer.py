1192df892dd40bcd496090a3eb21898b7c03220f
adk23333 <2633103794@qq.com>
2024-04-11 21:54:33 +0800

    修改测试样例

--- tieba-admin-server/plugins/review/test_reviewer.py
+++ tieba-admin-server/plugins/review/test_reviewer.py
@@ -1,5 +1,7 @@
 import unittest
 
+from tortoise import Tortoise
+
 from .reviewer import Reviewer
 
 
@@ -7,9 +9,15 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
     def __init__(self, method_name='runTest'):
         super().__init__(method_name)
 
+    async def test_init(self):
+        await Tortoise.init(db_url="sqlite://../../.cache/db.sqlite",
+                            modules={"models": ["core.models", "review.models"]})
+        await Reviewer.init_plugin()
+
     async def test_run(self):
         reviewer = Reviewer()
         reviewer.PLUGIN_MODEL = "review.models"
+
         await reviewer._start_plugin_with_process(
             db_url="sqlite://../../.cache/db.sqlite",
             log_level="DEBUG",
