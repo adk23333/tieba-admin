38750ad30692280fa21fa9ef58e4f0ddeb0bf56d
adk23333 <2633103794@qq.com>
2024-04-12 20:15:25 +0800

    统一进行exception_handle fix.1

--- tieba-admin-server/core/exception.py
+++ tieba-admin-server/core/exception.py
@@ -5,5 +5,5 @@ class ArgException(SanicException):
     status_code = 400
     message = "参数错误"
 
-    def __init__(self, message: str):
+    def __init__(self, message: str = "参数错误"):
         self.message = message
