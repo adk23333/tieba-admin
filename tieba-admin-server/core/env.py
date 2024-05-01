a021cc4cdd1567a860ad7c7a2a8446f6fff2bf13
adk23333 <2633103794@qq.com>
2024-05-01 17:52:20 +0800

    将环境变量的读取集中到env.py

---
+++ tieba-admin-server/core/env.py
@@ -0,0 +1,16 @@
+﻿import environs
+
+CACHE_PATH = "./.cache"
+CACHE_FILE = "db.sqlite"
+
+env = environs.Env()
+env.read_env(recurse=False)
+
+HOST = env.str("HOST", "0.0.0.0")
+PORT = env.int("PORT", 3100)
+WORKERS = env.int("WORKERS", 1)
+WEB = env.bool("WEB", True)
+SECRET = env.str("SECRET", "This is a big secret!!!")
+DB_URL = env.str("DB_URL", f"sqlite://{CACHE_PATH}/{CACHE_FILE}")
+DEV = env.bool("DEV", False)
+TZ = env.str("TZ", "Asia/Shanghai")
